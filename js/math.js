182f6d680e2f2821188b7916698070977d94a4e4
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-04 20:59:00 -0700

    Add files via upload
    Update05

--- js/math.js
+++ js/math.js
@@ -1,11 +1,34 @@
 /*eslint-env browser*/
 
 //STEP 1
+/*
+var num = prompt("Enter a number.");
+window.console.log(Math.abs(num));
+*/
 
 //STEP 2
+/*
+var num = prompt("Enter a number with a decimal point.");
+window.console.log(Math.ceil(num));
+*/
 
 //STEP 3
+/*
+var num = prompt("Enter a number with a decimal point.");
+window.console.log(Math.floor(num));
+*/
 
 //STEP 4
+/* COME BACK TO
+var nums = prompt("Enter five numbers. Separate them with a comma.");
+var five = nums;
+var max = (Math.max(five));
+var min = (Math.min(five));
+window.console.log(max);
+*/
 
 //STEP 5
+/*
+var num = prompt("Enter a number.");
+window.console.log(Math.sqrt(num));
+*/
