6c1713020381ebaf9901111163688e3d3bf7f6fa
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-04 19:25:05 -0700

    Add files via upload
    Update02

--- js/strings.js
+++ js/strings.js
@@ -1,16 +1,42 @@
 /*eslint-env browser*/
 
 //STEP 1
+/*
+var name = prompt("What is your name?");
+window.alert("There are " + (name.length) + " characters in your name.");
+*/
 
 //STEP 2
+/*
+var name = prompt("What is your name?");
+var num = prompt("Select a number.");
+window.alert(name.charAt(0));
+*/
 
 //STEP 3
+/*
+var fname = prompt("What is your first name?");
+var lname = prompt("What is your last name?");
+window.alert("Your name is: " + (fname.concat(lname)));
+*/
 
 //STEP 4
+/*
+var fox = "The quick brown fox jumps over the lazy dog.";
+window.alert(fox.indexOf("fox"));
+*/
 
 //STEP 5
+/*
+var fox = "The quick brown fox jumps over the lazy fox.";
+window.alert(fox.lastIndexOf("fox"));
+*/
 
 //STEP 6
+
+var dog = "The quick brown fox jumped over the lazy dog.";
+var fullname = prompt("What is your full name?");
+window.alert(dog.replace("the lazy dog", fullname);
 
 //STEP 7
 
