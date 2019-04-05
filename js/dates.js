c5fc2548fb19635e82d8005db2600ee9ba100653
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-04 21:36:38 -0700

    Add files via upload
    Update07

--- js/dates.js
+++ js/dates.js
@@ -2,6 +2,13 @@
 
 //STEP 1
 
+function daysInMonth(month, year) {
+    "use strict";
+    return new Date(year, month, 0).getDate();
+}
+daysInMonth(7, 2019);
+//window.console.log(d.getDate(month));
+
 //STEP 2
 
 //STEP 3
